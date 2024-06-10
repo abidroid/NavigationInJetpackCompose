16a9544d34646e138a0bf59f5b440cce71422ccf
Muhammad Abid <abidroid1@gmail.com>
2024-06-10 22:11:31 +0500

    main screen appbar

--- app/src/main/java/com/example/navigationinjetpackcompose/MainScreen.kt
+++ app/src/main/java/com/example/navigationinjetpackcompose/MainScreen.kt
@@ -5,17 +5,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     Scaffold(
+
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Home Screen") },
+                modifier = Modifier.background(color = Color.Blue),
+            ) },
+
         content = {
             Column(
                 modifier = Modifier
