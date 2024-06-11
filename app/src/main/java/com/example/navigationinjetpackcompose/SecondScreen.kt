964966f146d90a9e0fdab21574b00868f3f3725b
Muhammad Abid <abidroid1@gmail.com>
2024-06-11 17:32:34 +0500

    second screen updated

--- app/src/main/java/com/example/navigationinjetpackcompose/SecondScreen.kt
+++ app/src/main/java/com/example/navigationinjetpackcompose/SecondScreen.kt
@@ -5,23 +5,33 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SecondScreen() {
     Scaffold(
-
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Second Screen", color = Color.White) },
+                colors = topAppBarColors(Color.Blue)
+            )
+        },
         content = {
-            Column(modifier = Modifier
-                .padding(it)
-                .fillMaxSize()
-                .background(color = Color.White),
+            Column(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+                    .background(color = Color.White),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
