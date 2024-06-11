1784c820f28ee4c05ae703067d533b163d47c741
Muhammad Abid <abidroid1@gmail.com>
2024-06-11 17:15:59 +0500

    third screen added

---
+++ app/src/main/java/com/example/navigationinjetpackcompose/ThirdScreen.kt
@@ -0,0 +1,49 @@
+package com.example.navigationinjetpackcompose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ThirdScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Third Screen", color = Color.White) },
+                colors = topAppBarColors(Color.Blue)
+            ) },
+        content = {
+
+            Column(
+                modifier = Modifier
+                    .padding(it)
+                    .background(color = Color.White)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text("Third Screen Content")
+            }
+        }
+    )
+
+}
+
+@Preview
+@Composable
+fun ThirdScreenPreview() {
+    ThirdScreen()
+}
