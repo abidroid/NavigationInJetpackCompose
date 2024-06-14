ef7fd8e13548d99256da0d8404d6579965d986c3
Muhammad Abid <abidroid1@gmail.com>
2024-06-14 07:59:54 +0500

    empty arguement check

--- app/src/main/java/com/example/navigationinjetpackcompose/MainScreen.kt
+++ app/src/main/java/com/example/navigationinjetpackcompose/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.navigationinjetpackcompose
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,7 +14,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -21,7 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -32,6 +32,9 @@ fun MainScreen(navController: NavController) {
     val txtName = remember {
         mutableStateOf("")
     }
+
+    var myContext = LocalContext.current
+
     Scaffold(
 
         topBar = {
@@ -72,11 +75,17 @@ fun MainScreen(navController: NavController) {
 
                 TextField(value = txtName.value, onValueChange = { text: String ->
                     txtName.value = text
-                } )
+                })
                 Spacer(modifier = Modifier.height(16.dp))
                 Button(onClick = {
+
+                    if (txtName.value.isEmpty()) {
+                        Toast.makeText(myContext, "Please enter your name", Toast.LENGTH_SHORT).show()
+                    } else {
                         navController.navigate("FourthScreen/${txtName.value}")
-                }){
+
+                    }
+                }) {
                     Text("Go to Fourth Screen")
                 }
             }
